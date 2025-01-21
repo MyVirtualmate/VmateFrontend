4afe052878d9706880f43d236a341efc7d3d43a7
조유민 <nationmilo@gmail.com>
2025-01-22 03:12:43 +0900

    feat: sample mate list

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import '$lib/app.css';
+
+  const { children } = $props();
 </script>
 
-{@render children()}
+<header>
+  <a href="/">slog</a>
+  <a href="/member/login">로그인</a>
+  <a href="/member/join">회원가입</a>
+  <a href="/p/list">오늘의 버튜버</a>
+</header>
+<main>{@render children()}</main>
+<footer>푸터</footer>
---
+++ src/routes/p/list/+page.svelte
@@ -0,0 +1,17 @@
+﻿<script lang="ts">
+	async function load() {
+		return await fetch('http://localhost:8090/api/v1/posts').then((res) => res.json());
+	}
+</script>
+{#await load()}
+	<p>loading...</p>
+{:then data}
+	<h1>오늘의 버튜버</h1>
+	<ul>
+		{#each data.data.items as post}
+			<li>
+				<a href="/p/{post.id}">{post.title}</a>
+			</li>
+		{/each}
+	</ul>
+{/await}
